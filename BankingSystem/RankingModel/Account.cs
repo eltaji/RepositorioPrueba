c4c1a83ac24b84e59972cfb35461a1b9cdbdebbd
Clase <Clase>
2017-03-08 19:24:35 -0500

    Creacion de Proyecto Base

---
+++ BankingSystem/RankingModel/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingModel
+{
+    public class Account
+    {
+        private String _name;
+        public String getName() {
+        return _name;
+    
+    }
+        public void setName(String name)
+ {
+
+     _name = name;
+        }
+  }
+}
